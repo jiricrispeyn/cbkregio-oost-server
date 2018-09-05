266fd37603d8ff8a062b34aad527193f208d37bb
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-09-06 00:14:12 +0200

    Make addresses request an arrow fn

--- src/routes/leagues.js
+++ src/routes/leagues.js
@@ -1,13 +1,11 @@
-const express = require('express');
 const request = require('request');
 const cheerio = require('cheerio');
-const app = express();
 
 module.exports = app => {
   app.get('/leagues', (req, res) => {
     const url = 'http://cbkregio-oost.be/index.php?page=competitie';
 
-    request(url, function(error, response, html) {
+    request(url, (error, response, html) => {
       if (!error) {
         const $ = cheerio.load(html);
         let leagues = [];
