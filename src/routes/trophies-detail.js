5ef50a62fb95bca7f1ecea237cfa15e178ff0133
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-09-06 01:18:51 +0200

    Refactor basic throphies fns

--- src/routes/trophies-detail.js
+++ src/routes/trophies-detail.js
@@ -4,107 +4,128 @@ var cheerio = require('cheerio');
 var moment = require('moment');
 var app = express();
 
-module.exports = function (app) {
-  app.get('/trophies/:id', function (req, res) {
+module.exports = function(app) {
+  app.get('/trophies/:id', function(req, res) {
     // var url = 'http://cbkregio-oost.be/index.php?page=beker&detail=' + req.params.id;
-    var url = 'https://web.archive.org/web/20150313044238/http://cbkregio-oost.be/index.php?page=beker&detail=1';
+    var url =
+      'https://web.archive.org/web/20150313044238/http://cbkregio-oost.be/index.php?page=beker&detail=1';
 
-    request(url, function (error, response, html) {
+    request(url, function(error, response, html) {
       if (!error) {
         var $ = cheerio.load(html);
         var rounds = [];
 
-        $('.bekertbl').each(function (i, element) {
+        $('.bekertbl').each(function(i, element) {
           var round = {
             date: null,
             games: []
           };
 
-          $(this).find('tr').each(function(j, element) {
-            var content = $(this).find('.content, .contentwhite');
-            var contentscore = $(this).find('.contentscore, .contentscorewhite');
-            var row = {};
-            
-            if (content.length === 0 || contentscore.length === 0) {
-              return;
-            }
+          $(this)
+            .find('tr')
+            .each(function(j, element) {
+              var content = $(this).find('.content, .contentwhite');
+              var contentscore = $(this).find(
+                '.contentscore, .contentscorewhite'
+              );
+              var row = {};
 
-            $(this).find('td').each(function (k, element) {
-              var key;
-              var team;
-
-              switch(k) {
-                case 0:
-                case 3: key = 'league';
-                  break;
-                case 1:
-                case 4: key = 'club';
-                  break;
-                case 2:
-                case 5: key = 'city';
-                  break;
-                case 6: key = 'result';
-                  break;
-                case 7: key = 'result_test_game';
-                  break;
+              if (content.length === 0 || contentscore.length === 0) {
+                return;
               }
 
-              switch (k) {
-                case 0:
-                case 1:
-                case 2: team = 'home';
-                  break;
-                case 3:
-                case 4:
-                case 5: team = 'away';
-                  break;
-              }
+              $(this)
+                .find('td')
+                .each(function(k, element) {
+                  var key;
+                  var team;
 
-              if (key) {
-                var text = $(this).text();
-                
-                if (k === 1 || k === 2 || k === 4 || k === 5) {
-                  text = text.replace(/[\n\t\r]/g, '').trim();
-                }
-
-                if (k === 7 && text.trim().length === 0) {
-                  text = '';
-                }
-
-                if (team) {
-                  if (!row[team]) {
-                    row[team] = {};
+                  switch (k) {
+                    case 0:
+                    case 3:
+                      key = 'league';
+                      break;
+                    case 1:
+                    case 4:
+                      key = 'club';
+                      break;
+                    case 2:
+                    case 5:
+                      key = 'city';
+                      break;
+                    case 6:
+                      key = 'result';
+                      break;
+                    case 7:
+                      key = 'result_test_game';
+                      break;
                   }
 
-                  row[team][key] = text;
-
-                  if (key === 'club') {
-                    row[team]['won'] = $(this).find('img').length > 0 ? true : false;
+                  switch (k) {
+                    case 0:
+                    case 1:
+                    case 2:
+                      team = 'home';
+                      break;
+                    case 3:
+                    case 4:
+                    case 5:
+                      team = 'away';
+                      break;
                   }
-                } else {
-                  row[key] = text;
-                }
-              }
 
-              if (k === 8) {
-                var scoresheet = $(this).find('a').attr('href');
-                var scoresheet_id = null;
-                var scoresheet_url = null;
+                  if (key) {
+                    var text = $(this).text();
 
-                if (scoresheet) {
-                  scoresheet_id = scoresheet.split('id=')[1];
-                  scoresheet_url = 'http://cbkregio-oost.be/' + scoresheet;
-                }
+                    if (k === 1 || k === 2 || k === 4 || k === 5) {
+                      text = text.replace(/[\n\t\r]/g, '').trim();
+                    }
 
-                row.scoresheet_id = scoresheet_id;
-                row.scoresheet_url = scoresheet_url;
-              }
+                    if (k === 7 && text.trim().length === 0) {
+                      text = '';
+                    }
+
+                    if (team) {
+                      if (!row[team]) {
+                        row[team] = {};
+                      }
+
+                      row[team][key] = text;
+
+                      if (key === 'club') {
+                        row[team]['won'] =
+                          $(this).find('img').length > 0 ? true : false;
+                      }
+                    } else {
+                      row[key] = text;
+                    }
+                  }
+
+                  if (k === 8) {
+                    var scoresheet = $(this)
+                      .find('a')
+                      .attr('href');
+                    var scoresheet_id = null;
+                    var scoresheet_url = null;
+
+                    if (scoresheet) {
+                      scoresheet_id = scoresheet.split('id=')[1];
+                      scoresheet_url = 'http://cbkregio-oost.be/' + scoresheet;
+                    }
+
+                    row.scoresheet_id = scoresheet_id;
+                    row.scoresheet_url = scoresheet_url;
+                  }
+                });
+
+              round.games.push(row);
             });
 
-            round.games.push(row);
-          });
-
-          var date = $(this).closest('tr').prev().find('td').text();
+          var date = $(this)
+            .closest('tr')
+            .prev()
+            .find('td')
+            .text();
 
           if (moment(date, 'DD-MM-YYYY').isValid()) {
             round.date = date;
@@ -115,9 +136,9 @@ module.exports = function (app) {
 
         res.send({
           id: req.params.id,
-          rounds: rounds
+          rounds
         });
       }
     });
   });
-}
+};
