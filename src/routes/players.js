51422ddac282baae6de9bc835d52d08e3a3b44ff
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-09-06 00:17:04 +0200

    Refactor players route to ES6 approach

--- src/routes/players.js
+++ src/routes/players.js
@@ -1,51 +1,61 @@
-var express = require('express');
-var request = require('request');
-var cheerio = require('cheerio');
-var app = express();
+const request = require('request');
+const cheerio = require('cheerio');
 
-module.exports = function (app) {
+module.exports = app => {
   app.get('/players/:league', (req, res) => {
-    var url = 'http://cbkregio-oost.be/index.php?page=spelerslijst&afdeling=' + req.params.league;
+    let url = `http://cbkregio-oost.be/index.php?page=spelerslijst&afdeling=${
+      req.params.league
+    }`;
 
-    request(url, function (error, response, html) {
+    request(url, (error, response, html) => {
       if (!error) {
-        var $ = cheerio.load(html);
-        var players = [];
+        const $ = cheerio.load(html);
+        let players = [];
 
-        $('.nieuwstbl tr.spelerslijsteven, .nieuwstbl tr.spelerslijstodd').each(function(i, element) {
-          var row = {};
+        $('.nieuwstbl tr.spelerslijsteven, .nieuwstbl tr.spelerslijstodd').each(
+          function() {
+            let row = {};
 
-          $(this).find('td').each(function (j, element) {
-            var key;
+            $(this)
+              .find('td')
+              .each(function(j) {
+                let key;
 
-            switch (j) {
-              case 1: key = 'last_name';
-                break;
-              case 2: key = 'first_name';
-                break;
-              case 3: key = 'club';
-                break;
-              case 4: key = 'id';
-                break;
-              case 5: key = 'birthdate';
-                break;
-              case 6: key = 'ranking';
-                break;
-            }
-                  
-            if (key) {
-              row[key] = $(this).text();
-            }
-          });
+                switch (j) {
+                  case 1:
+                    key = 'last_name';
+                    break;
+                  case 2:
+                    key = 'first_name';
+                    break;
+                  case 3:
+                    key = 'club';
+                    break;
+                  case 4:
+                    key = 'id';
+                    break;
+                  case 5:
+                    key = 'birthdate';
+                    break;
+                  case 6:
+                    key = 'ranking';
+                    break;
+                }
 
-          players.push(row);
-        });
+                if (key) {
+                  row[key] = $(this).text();
+                }
+              });
+
+            players = [...players, row];
+          }
+        );
 
         res.send({
           league: req.params.league,
-          players: players
+          players
         });
       }
     });
   });
-}
+};
