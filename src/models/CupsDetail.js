bd568a2c5288c3bbc7e4100920f966141f587b51
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-10-05 01:24:37 +0200

    Add CupsDetail model

---
+++ src/models/CupsDetail.js
@@ -0,0 +1,18 @@
+const LEAGUE = 'league';
+const CLUB = 'club';
+const CITY = 'city';
+const SCORE = 'score';
+const TIEBREAKER = 'tiebreaker';
+const SCORESHEET_ID = 'scoresheet_id';
+
+module.exports = {
+  key0: LEAGUE,
+  key1: CLUB,
+  key2: CITY,
+  key3: LEAGUE,
+  key4: CLUB,
+  key5: CITY,
+  key6: SCORE,
+  key7: TIEBREAKER,
+  key8: SCORESHEET_ID,
+};
