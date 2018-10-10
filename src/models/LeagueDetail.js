858400cc8fb89dcee7efe3b6b49ea1055dc44425
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-10-11 01:21:59 +0200

    Create LATEST_RESULTS key for LEagueDetail model

---
+++ src/models/LeagueDetail.js
@@ -0,0 +1,12 @@
+const CLUB = 'club';
+const SCORE = 'score';
+const COMMENT = 'comment';
+
+const LATEST_RESULTS = {
+  key0: CLUB,
+  key2: CLUB,
+  key3: SCORE,
+  key4: COMMENT,
+};
+
+module.exports = { LATEST_RESULTS };
