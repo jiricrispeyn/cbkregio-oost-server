76ad38e23bbe175fadc7593e04e1711c13e54e50
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-04-17 01:57:14 +0200

    Add club to scoresheet

--- src/routes/league-scoresheet.js
+++ src/routes/league-scoresheet.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const moment = require('moment');
+const { HOME, AWAY } = require('../models/Teams');
 
 module.exports = app => {
   app.get('/leagues/:league/scoresheet/:id', (req, res) => {
@@ -12,10 +13,14 @@ module.exports = app => {
         const $ = cheerio.load(html);
 
         let date = null;
+        let response = {};
 
         $('.klassementtbl > tbody > tr').each(function(i) {
           if (i === 0) {
-            const text = $(this).text().trim().split(' ')[1];
+            const text = $(this)
+              .text()
+              .trim()
+              .split(' ')[1];
 
             if (moment(text, 'DD-MM-YYYY').isValid()) {
               date = text;
@@ -23,16 +28,37 @@ module.exports = app => {
           }
 
           if (i === 1) {
-          }
+            $(this)
+              .find('td')
+              .each(function(j) {
+                let team;
 
-          
+                if (j === 0) {
+                  team = HOME;
+                } else if (j === 1) {
+                  team = AWAY;
+                }
+
+                if (team) {
+                  const text = $(this)
+                    .text()
+                    .trim()
+                    .split(' : ')[1];
+
+                  response[team] = {
+                    ...response[team],
+                    club: text,
+                  };
+                }
+              });
+          }
         });
 
         res.send({
-          date
-        })
+          date,
+          ...response,
+        });
       }
-    })
-
-  })
-}
+    });
+  });
+};
