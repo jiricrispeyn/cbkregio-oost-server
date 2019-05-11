20c76f727862ac14ef144b05061c14f03e0ca7f1
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-05-11 18:19:24 +0200

    Refactor league scoresheet with better json structure

---
+++ src/models/Scoresheet.js
@@ -0,0 +1,11 @@
+const DATE = 'date';
+const NAME = 'name';
+
+const SCORESHEET = {
+  key0: DATE,
+  key1: NAME,
+};
+
+const HISTORY = {};
+
+module.exports = { SCORESHEET, HISTORY };
--- src/routes/league-scoresheet.js
+++ src/routes/league-scoresheet.js
@@ -3,6 +3,7 @@ const cheerio = require('cheerio');
 const moment = require('moment');
 const groupBy = require('lodash.groupby');
 const { HOME, AWAY } = require('../models/Teams');
+const { SCORESHEET } = require('../models/Scoresheet');
 const { getScores, getWinner } = require('../utils/matches');
 
 const pairs = {
@@ -19,6 +20,19 @@ const pairs = {
   13: 'R',
 };
 
+function getDate(node) {
+  const text = node
+    .text()
+    .trim()
+    .split(' ')[1];
+
+  if (!moment(text, 'DD-MM-YYYY').isValid()) {
+    return null;
+  }
+
+  return text;
+}
+
 module.exports = app => {
   app.get('/leagues/:league/scoresheet/:id', (req, res) => {
     const { league, id } = req.params;
@@ -29,53 +43,40 @@ module.exports = app => {
         const $ = cheerio.load(html);
 
         let date = null;
-        let response = {};
-
-        let players = {
-          [HOME]: [],
-          [AWAY]: [],
+        let clubs = {
+          [HOME]: {
+            lineup: [],
+          },
+          [AWAY]: {
+            lineup: [],
+          },
         };
         let playersIdx = 0;
 
+        let response = {};
         let sets = [];
 
         $('.klassementtbl > tbody > tr').each(function(i) {
           if (i === 0) {
-            const text = $(this)
-              .text()
-              .trim()
-              .split(' ')[1];
-
-            if (moment(text, 'DD-MM-YYYY').isValid()) {
-              date = text;
-            }
+            date = getDate($(this));
           }
 
           if (i === 1) {
+            const key = SCORESHEET[`key${i}`];
+
             $(this)
               .find('td')
               .each(function(j) {
-                let team;
-
-                if (j === 0) {
-                  team = HOME;
-                } else if (j === 1) {
-                  team = AWAY;
-                }
-
-                if (!team) {
-                  return;
-                }
-
                 const text = $(this)
                   .text()
                   .trim()
                   .split(' : ')[1];
 
-                response[team] = {
-                  ...response[team],
-                  club: text,
-                };
+                if (j === 0) {
+                  clubs[HOME][key] = text;
+                } else if (j === 1) {
+                  clubs[AWAY][key] = text;
+                }
               });
           }
 
@@ -85,6 +86,8 @@ module.exports = app => {
             if (!pair) {
               return;
             }
+
+            console.log({ pair });
 
             $(this)
               .find('td')
@@ -117,86 +120,175 @@ module.exports = app => {
                   text = null;
                 }
 
-                players[team][playersIdx] = players[team][playersIdx] || {};
-                players[team][playersIdx][key] = text;
+                clubs[team].lineup[playersIdx] = clubs[team].lineup[
+                  playersIdx
+                ] || {
+                  pair,
+                };
+                clubs[team].lineup[playersIdx][key] = text;
               });
-
-            players[HOME][playersIdx] = { ...players[HOME][playersIdx], pair };
-            players[AWAY][playersIdx] = { ...players[AWAY][playersIdx], pair };
 
             ++playersIdx;
+
+            // $(this)
+            //   .find('td')
+            //   .each(function(j) {
+            //     let team;
+
+            //     if (j === 1 || j === 2) {
+            //       team = HOME;
+            //     } else if (j === 4 || j === 5) {
+            //       team = AWAY;
+            //     }
+
+            //     if (!team) {
+            //       return;
+            //     }
+
+            //     let key;
+            //     let text = $(this)
+            //       .text()
+            //       .trim();
+
+            //     if (j === 1 || j === 4) {
+            //       key = 'name';
+            //     } else if (j === 2 || j === 5) {
+            //       key = 'id';
+            //       text = text.slice(1, -1);
+            //     }
+
+            //     if (text.length === 0) {
+            //       text = null;
+            //     }
+
+            //     clubs[team][playersIdx] = players[team][playersIdx] || {};
+            //     clubs[team][playersIdx][key] = text;
+            //   });
+
+            // clubs[HOME][playersIdx] = { ...players[HOME][playersIdx], pair };
+            // clubs[AWAY][playersIdx] = { ...players[AWAY][playersIdx], pair };
+
+            // ++playersIdx;
           }
 
-          if (i === 14) {
-            $(this)
-              .find('table > tbody > tr')
-              .each(function(j) {
-                if (j >= 2 && j <= 10) {
-                  let set_number;
-                  let results = {
-                    [HOME]: {},
-                    [AWAY]: {},
-                  };
+          // if (i >= 2 && i <= 13) {
+          //   const pair = pairs[i];
 
-                  $(this)
-                    .find('td')
-                    .each(function(k) {
-                      let text = $(this).text();
+          //   if (!pair) {
+          //     return;
+          //   }
 
-                      console.log({ [k]: text });
+          //   $(this)
+          //     .find('td')
+          //     .each(function(j) {
+          //       let team;
 
-                      if (k === 0) {
-                        set_number = parseInt(text);
-                      } else if (k === 1) {
-                        text = text.replace(/[\n\t\r\']/g, '').trim();
+          //       if (j === 1 || j === 2) {
+          //         team = HOME;
+          //       } else if (j === 4 || j === 5) {
+          //         team = AWAY;
+          //       }
 
-                        const [pairHome, pairAway] = text.split(' -');
+          //       if (!team) {
+          //         return;
+          //       }
 
-                        results[HOME].pair = pairHome;
-                        results[AWAY].pair = pairAway;
-                      } else if (k === 2) {
-                        const scores = getScores(text);
+          //       let key;
+          //       let text = $(this)
+          //         .text()
+          //         .trim();
 
-                        results[HOME].score = isNaN(scores[HOME])
-                          ? null
-                          : scores[HOME];
-                        results[AWAY].score = isNaN(scores[AWAY])
-                          ? null
-                          : scores[AWAY];
-                      } else if (k === 3) {
-                        const [setpointsHome, setpointsAway] = text.split('-');
+          //       if (j === 1 || j === 4) {
+          //         key = 'name';
+          //       } else if (j === 2 || j === 5) {
+          //         key = 'id';
+          //         text = text.slice(1, -1);
+          //       }
 
-                        results[HOME].setpoints = parseInt(setpointsHome);
-                        results[AWAY].setpoints = parseInt(setpointsAway);
-                      }
-                    });
+          //       if (text.length === 0) {
+          //         text = null;
+          //       }
 
-                  const winner = getWinner(results[HOME], results[AWAY]);
+          //       players[team][playersIdx] = players[team][playersIdx] || {};
+          //       players[team][playersIdx][key] = text;
+          //     });
 
-                  sets = [
-                    ...sets,
-                    {
-                      set_number,
-                      ...results,
-                      winner,
-                    },
-                  ];
-                }
-              });
-          }
+          //   players[HOME][playersIdx] = { ...players[HOME][playersIdx], pair };
+          //   players[AWAY][playersIdx] = { ...players[AWAY][playersIdx], pair };
 
-          response[HOME] = {
-            ...response[HOME],
-            lineup: groupBy(players[HOME].filter(player => player.id), 'pair'),
-          };
-          response[AWAY] = {
-            ...response[AWAY],
-            lineup: groupBy(players[AWAY].filter(player => player.id), 'pair'),
-          };
+          //   ++playersIdx;
+          // }
+
+          // if (i === 14) {
+          //   $(this)
+          //     .find('table > tbody > tr')
+          //     .each(function(j) {
+          //       if (j >= 2 && j <= 10) {
+          //         let set_number;
+          //         let results = {
+          //           [HOME]: {},
+          //           [AWAY]: {},
+          //         };
+
+          //         $(this)
+          //           .find('td')
+          //           .each(function(k) {
+          //             let text = $(this).text();
+
+          //             if (k === 0) {
+          //               set_number = parseInt(text);
+          //             } else if (k === 1) {
+          //               text = text.replace(/[\n\t\r\']/g, '').trim();
+
+          //               const [pairHome, pairAway] = text.split(' -');
+
+          //               results[HOME].pair = pairHome;
+          //               results[AWAY].pair = pairAway;
+          //             } else if (k === 2) {
+          //               const scores = getScores(text);
+
+          //               results[HOME].score = isNaN(scores[HOME])
+          //                 ? null
+          //                 : scores[HOME];
+          //               results[AWAY].score = isNaN(scores[AWAY])
+          //                 ? null
+          //                 : scores[AWAY];
+          //             } else if (k === 3) {
+          //               const [setpointsHome, setpointsAway] = text.split('-');
+
+          //               results[HOME].setpoints = parseInt(setpointsHome);
+          //               results[AWAY].setpoints = parseInt(setpointsAway);
+          //             }
+          //           });
+
+          //         const winner = getWinner(results[HOME], results[AWAY]);
+
+          //         sets = [
+          //           ...sets,
+          //           {
+          //             set_number,
+          //             ...results,
+          //             winner,
+          //           },
+          //         ];
+          //       }
+          //     });
+          // }
+
+          // response[HOME] = {
+          //   ...response[HOME],
+          //   lineup: groupBy(players[HOME].filter(player => player.id), 'pair'),
+          // };
+          // response[AWAY] = {
+          //   ...response[AWAY],
+          //   lineup: groupBy(players[AWAY].filter(player => player.id), 'pair'),
+          // };
         });
 
         res.send({
+          id,
           date,
+          clubs,
           ...response,
           sets,
         });
