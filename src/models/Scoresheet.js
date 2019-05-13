1b7d14dbd53258c643d7c1c1a6b0deed22a23cd6
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2019-05-13 23:49:48 +0200

    Create SETS model

--- src/models/Scoresheet.js
+++ src/models/Scoresheet.js
@@ -1,11 +1,20 @@
 const DATE = 'date';
 const NAME = 'name';
+const SET_NUMBER = 'set_number';
+const PAIR = 'pair';
+const SCORE = 'score';
+const SETPOINTS = 'setpoints';
 
 const SCORESHEET = {
   key0: DATE,
   key1: NAME,
 };
 
-const HISTORY = {};
+const SETS = {
+  key0: SET_NUMBER,
+  key1: PAIR,
+  key2: SCORE,
+  key3: SETPOINTS,
+};
 
-module.exports = { SCORESHEET, HISTORY };
+module.exports = { SCORESHEET, SETS };
